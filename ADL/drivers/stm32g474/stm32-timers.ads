b0fdd607b9daf68255bbdc32a3941b62fef6fa76
Julio Gobbi <jcgobbi@yandex.com>
2021-10-24 13:40:16 -0300

    Add new options to Timer_Slave_Mode type.

--- ADL/drivers/stm32g474/stm32-timers.ads
+++ ADL/drivers/stm32g474/stm32-timers.ads
@@ -969,16 +969,37 @@ package STM32.Timers is
 
    type Timer_Slave_Mode is
      (Disabled,
-      --  counter counts up/down on TI1 edge
       Encoder_Mode_TI1,
-      --  counter counts up/down on TI2 edge
+      --  Counter counts up/down on TI1 edge.
       Encoder_Mode_TI2,
-      --  counter counts up/down on both TI1 & TI2 edges
+      --  Counter counts up/down on TI2 edge.
       Encoder_Mode_TI1_TI2,
+      --  Counter counts up/down on both TI1 & TI2 edges.
       Reset,
+      --  Counter reinitialize and update registers on TRGI rising edge.
       Gated,
+      --  Counter is enabled when TRGI is high and stops when low without reset.
       Trigger,
-      External_1);
+      --  Counter starts at TRGI trigger rising edge.
+      External_1,
+      --  Counter clocked by TRGI rising edges.
+      Combined_Reset_Trigger,
+      --  Counter reinitialize, update registers and start the counter on TRGI.
+      Combined_Reset_Gated,
+      --  Counter reinitialize, update registers and start the counter on TRGI.
+      Encoder_Mode_1,
+      --  Clock plus direction, x2 mode.
+      Encoder_Mode_2,
+      --  Clock plus direction, x1 mode.
+      Encoder_Mode_3,
+      --  Directional clock, x2 mode.
+      Encoder_Mode_4,
+      --  Directional clock, x1 mode.
+      Quadrature_Mode_1,
+      --  x1 mode, counting on tim_ti1fp1 edges only.
+      Quadrature_Mode_2)
+      --  x1 mode, counting on tim_ti2fp2 edges only.
+    with Size => 4;
 
    procedure Select_Slave_Mode
      (This : in out Timer;
@@ -991,13 +1012,13 @@ package STM32.Timers is
    procedure Disable_Master_Slave_Mode (This : in out Timer)
      with Pre => Slave_Mode_Supported (This);
 
-   type Timer_External_Trigger_Polarity is (Inverted, Noninverted);
+   type Timer_External_Trigger_Polarity is (NonInverted, Inverted);
 
    type Timer_External_Trigger_Prescaler is
      (Off,
-      Div2,
-      Div4,
-      Div8);
+      Div_2,
+      Div_4,
+      Div_8);
 
    type Timer_External_Trigger_Filter is
      (No_Filter,
@@ -1409,7 +1430,7 @@ private
       Reserved1                  : UInt2;
       Trigger_Selection_1        : UInt2; --  Timer_External_Trigger_Filter
       Reserved2                  : UInt3;
-      Slave_Mode_Selection_3     : Boolean;
+      Slave_Mode_Selection_2     : Boolean; --  Timer_Slave_Mode
       External_Trigger_Polarity  : Timer_External_Trigger_Polarity;
       External_Clock_Enable      : Boolean;
       External_Trigger_Prescaler : Timer_External_Trigger_Prescaler;
@@ -1417,7 +1438,7 @@ private
       Master_Slave_Mode          : Boolean;
       Trigger_Selection          : UInt3; --  Timer_External_Trigger_Filter
       OCREF_Clear_Selection      : Boolean;
-      Slave_Mode_Selection       : Timer_Slave_Mode;
+      Slave_Mode_Selection       : UInt3; --  Timer_Slave_Mode
    end record with Volatile_Full_Access, Size => 32;
 
    for TIMx_SMCR use record
@@ -1427,7 +1448,7 @@ private
       Reserved1                  at 0 range 22 .. 23;
       Trigger_Selection_1        at 0 range 20 .. 21;
       Reserved2                  at 0 range 17 .. 19;
-      Slave_Mode_Selection_3     at 0 range 16 .. 16;
+      Slave_Mode_Selection_2     at 0 range 16 .. 16;
       External_Trigger_Polarity  at 0 range 15 .. 15;
       External_Clock_Enable      at 0 range 14 .. 14;
       External_Trigger_Prescaler at 0 range 12 .. 13;
