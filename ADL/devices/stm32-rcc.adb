f3077eb9b13047273771c61d2faef1223f42a420
Julio Gobbi <jcgobbi@yandex.com>
2021-11-07 17:44:16 -0300

    Add routines to setup AHB, APB and all clocks.

--- ADL/devices/stm32-rcc.adb
+++ ADL/devices/stm32-rcc.adb
@@ -30,7 +30,10 @@
 ------------------------------------------------------------------------------
 
 with Ada.Unchecked_Conversion;
-with STM32_SVD.RCC; use STM32_SVD.RCC;
+with STM32_SVD.RCC;    use STM32_SVD.RCC;
+with STM32_SVD.PWR;    use STM32_SVD.PWR;
+with STM32_SVD.SYSCFG; use STM32_SVD.SYSCFG;
+with STM32_SVD.Flash;  use STM32_SVD.Flash;
 
 package body STM32.RCC is
 
@@ -86,5 +89,321 @@ package body STM32.RCC is
    begin
       RCC_Periph.APB2RSTR := To_APB2RSTR_T (0);
    end APB2_Release_Reset;
+
+   procedure Backup_Domain_Reset is
+   begin
+      RCC_Periph.BDCR.BDRST := True;
+      RCC_Periph.BDCR.BDRST := False;
+   end Backup_Domain_Reset;
+
+   ---------------------------------------------------------------------------
+   --  Clock Configuration  --------------------------------------------------
+   ---------------------------------------------------------------------------
+
+   -------------------
+   -- Set_HSE Clock --
+   -------------------
+
+   procedure Set_HSE_Clock
+     (Enable     : Boolean;
+      Bypass     : Boolean := False;
+      Enable_CSS : Boolean := False)
+   is
+   begin
+      if Enable and not RCC_Periph.CR.HSEON then
+         RCC_Periph.CR.HSEON := True;
+         loop
+            exit when RCC_Periph.CR.HSERDY;
+         end loop;
+      end if;
+      RCC_Periph.CR.HSEBYP := Bypass;
+      RCC_Periph.CR.CSSON := Enable_CSS;
+   end Set_HSE_Clock;
+
+   -----------------------
+   -- HSE Clock_Enabled --
+   -----------------------
+
+   function HSE_Clock_Enabled return Boolean is
+   begin
+      return RCC_Periph.CR.HSEON;
+   end HSE_Clock_Enabled;
+
+   -------------------
+   -- Set_LSE Clock --
+   -------------------
+
+   procedure Set_LSE_Clock
+     (Enable     : Boolean;
+      Bypass     : Boolean := False;
+      Enable_CSS : Boolean := False;
+      Capability : HSE_Capability)
+   is
+   begin
+      if Enable and not RCC_Periph.BDCR.LSEON then
+         RCC_Periph.BDCR.LSEON := True;
+         loop
+            exit when RCC_Periph.BDCR.LSERDY;
+         end loop;
+      end if;
+      RCC_Periph.BDCR.LSEBYP := Bypass;
+      RCC_Periph.BDCR.LSECSSON := Enable_CSS;
+      RCC_Periph.BDCR.LSEDRV := Capability'Enum_Rep;
+   end Set_LSE_Clock;
+
+   -----------------------
+   -- LSE Clock_Enabled --
+   -----------------------
+
+   function LSE_Clock_Enabled return Boolean is
+   begin
+      return RCC_Periph.BDCR.LSEON;
+   end LSE_Clock_Enabled;
+
+   -------------------
+   -- Set_HSI_Clock --
+   -------------------
+
+   procedure Set_HSI_Clock (Enable : Boolean) is
+   begin
+      if Enable then
+         if not RCC_Periph.CR.HSION then
+            RCC_Periph.CR.HSION := True;
+            loop
+               exit when RCC_Periph.CR.HSIRDY;
+            end loop;
+         end if;
+      else
+         RCC_Periph.CR.HSION := False;
+      end if;
+   end Set_HSI_Clock;
+
+   -----------------------
+   -- HSI_Clock_Enabled --
+   -----------------------
+
+   function HSI_Clock_Enabled return Boolean is
+   begin
+      return RCC_Periph.CR.HSION;
+   end HSI_Clock_Enabled;
+
+   ---------------------
+   -- Set_HSI48 Clock --
+   ---------------------
+
+   procedure Set_HSI48_Clock (Enable : Boolean) is
+   begin
+      if Enable and not RCC_Periph.CRRCR.HSI48ON then
+         RCC_Periph.CRRCR.HSI48ON := True;
+         loop
+            exit when RCC_Periph.CRRCR.HSI48RDY;
+         end loop;
+      end if;
+   end Set_HSI48_Clock;
+
+   -------------------------
+   -- HSI48 Clock_Enabled --
+   -------------------------
+
+   function HSI48_Clock_Enabled return Boolean is
+   begin
+      return RCC_Periph.CRRCR.HSI48ON;
+   end HSI48_Clock_Enabled;
+
+   -------------------
+   -- Set_LSI Clock --
+   -------------------
+
+   procedure Set_LSI_Clock (Enable : Boolean) is
+   begin
+      if Enable and not RCC_Periph.CSR.LSION then
+         RCC_Periph.CSR.LSION := True;
+         loop
+            exit when RCC_Periph.CSR.LSIRDY;
+         end loop;
+      end if;
+   end Set_LSI_Clock;
+
+   -----------------------
+   -- LSI Clock_Enabled --
+   -----------------------
+
+   function LSI_Clock_Enabled return Boolean is
+   begin
+      return RCC_Periph.CSR.LSION;
+   end LSI_Clock_Enabled;
+
+   --------------------------------
+   -- Configure_System_Clock_Mux --
+   --------------------------------
+
+   procedure Configure_System_Clock_Mux (Source : SYSCLK_Source)
+   is
+   begin
+      RCC_Periph.CFGR.SW := Source'Enum_Rep;
+      loop
+         exit when RCC_Periph.CFGR.SWS = Source'Enum_Rep;
+      end loop;
+   end Configure_System_Clock_Mux;
+
+   -----------------------------------
+   -- Configure_AHB_Clock_Prescaler --
+   -----------------------------------
+
+   procedure Configure_AHB_Clock_Prescaler
+     (Value : AHB_Prescaler)
+   is
+      function To_AHB is new Ada.Unchecked_Conversion
+        (AHB_Prescaler, UInt4);
+   begin
+      RCC_Periph.CFGR.HPRE := To_AHB (Value);
+   end Configure_AHB_Clock_Prescaler;
+
+   -----------------------------------
+   -- Configure_APB_Clock_Prescaler --
+   -----------------------------------
+
+   procedure Configure_APB_Clock_Prescaler
+     (Bus   : APB_Clock_Range;
+      Value : APB_Prescaler)
+   is
+      function To_APB is new Ada.Unchecked_Conversion
+        (APB_Prescaler, UInt3);
+   begin
+      case Bus is
+         when APB_1 =>
+            RCC_Periph.CFGR.PPRE.Arr (1) := To_APB (Value);
+         when APB_2 =>
+            RCC_Periph.CFGR.PPRE.Arr (2) := To_APB (Value);
+      end case;
+   end Configure_APB_Clock_Prescaler;
+
+   ------------------------------
+   -- Configure_PLL_Source_Mux --
+   ------------------------------
+
+   procedure Configure_PLL_Source_Mux (Source : PLL_Source) is
+   begin
+         RCC_Periph.PLLCFGR.PLLSRC := Source'Enum_Rep;
+   end Configure_PLL_Source_Mux;
+
+   -------------------
+   -- Configure_PLL --
+   -------------------
+
+   procedure Configure_PLL
+     (Enable          : Boolean;
+      PLLM            : PLLM_Range;
+      PLLN            : PLLN_Range;
+      PLLP            : PLLP_Range;
+      Enable_Output_P : Boolean;
+      PLLQ            : PLLQ_Range;
+      Enable_Output_Q : Boolean;
+      PLLR            : PLLR_Range;
+      Enable_Output_R : Boolean)
+   is
+   begin
+      --  Disable the main PLL before configuring it
+      RCC_Periph.CR.PLLON := False;
+      loop
+         exit when not RCC_Periph.CR.PLLRDY;
+      end loop;
+
+      if Enable then
+         RCC_Periph.PLLCFGR :=
+           (PLLM    => UInt4 (PLLM - 1),
+            PLLN    => UInt7 (PLLN),
+            PLLPDIV => UInt5 (PLLP),
+            PLLPEN  => Enable_Output_P,
+            PLLQ    => UInt2 (PLLQ / 2 - 1),
+            PLLQEN  => Enable_Output_Q,
+            PLLR    => UInt2 (PLLR / 2 - 1),
+            PLLREN  => Enable_Output_R,
+            others  => <>);
+
+         --  Setup PLL and wait for stabilization.
+         RCC_Periph.CR.PLLON := Enable;
+         loop
+            exit when RCC_Periph.CR.PLLRDY;
+         end loop;
+      end if;
+   end Configure_PLL;
+
+   --------------------------------
+   -- Configure_MCO_Output_Clock --
+   --------------------------------
+
+   procedure Configure_MCO_Output_Clock
+     (Source : MCO_Source;
+      Value  : MCO_Prescaler)
+   is
+   begin
+      RCC_Periph.CFGR.MCOSEL := Source'Enum_Rep;
+      RCC_Periph.CFGR.MCOPRE := Value'Enum_Rep;
+   end Configure_MCO_Output_Clock;
+
+   ---------------------------------
+   -- Configure_LSCO_Output_Clock --
+   ---------------------------------
+
+   procedure Configure_LSCO_Output_Clock
+     (Enable : Boolean;
+      Source : LSCO_Source := LSCO_Source'First)
+   is
+   begin
+      if Enable then
+         RCC_Periph.BDCR.LSCOSEL := Source = LSE;
+         RCC_Periph.BDCR.LSCOEN := True;
+      else
+         RCC_Periph.BDCR.LSCOEN := False;
+      end if;
+   end Configure_LSCO_Output_Clock;
+
+   -----------------------
+   -- Set_FLASH_Latency --
+   -----------------------
+
+   procedure Set_FLASH_Latency (Latency : FLASH_Wait_State)
+   is
+   begin
+      FLASH_Periph.ACR.LATENCY := Latency'Enum_Rep;
+   end Set_FLASH_Latency;
+
+   -----------------------
+   -- Set_VCORE_Scaling --
+   -----------------------
+
+   procedure Set_VCORE_Scaling (Scale : VCORE_Scaling_Selection)
+   is
+   begin
+      PWR_Periph.CR5.R1MODE := Scale = Range_1;
+   end Set_VCORE_Scaling;
+
+   --------------------------
+   -- PWR_Overdrive_Enable --
+   --------------------------
+
+   procedure PWR_Overdrive_Enable
+   is
+   begin
+      --  Range 1 boost mode (R1MODE = 0) when SYSCLK ≤ 170 MHz.
+      --  Range 1 normal mode (R1MODE = 1) when SYSCLK ≤ 150 MHz.
+      --  See RM0440 ver. 4 pg. 235 chapter 6.1.5 for other modes.
+      if PWR_Periph.CR1.VOS = Range_2'Enum_Rep then
+         PWR_Periph.CR1.VOS := Range_1'Enum_Rep;
+         --  Wait for stabilization
+         loop
+            exit when not PWR_Periph.SR2.VOSF;
+         end loop;
+      end if;
+
+      PWR_Periph.CR5.R1MODE := True;
+      --  Main regulator in range 1 boost mode.
+      --  Wait for stabilization
+      loop
+         exit when not PWR_Periph.SR2.VOSF;
+      end loop;
+
+   end PWR_Overdrive_Enable;
 
 end STM32.RCC;
--- ADL/devices/stm32-rcc.ads
+++ ADL/devices/stm32-rcc.ads
@@ -39,4 +39,274 @@ package STM32.RCC is
    procedure APB2_Force_Reset with Inline;
    procedure APB2_Release_Reset with Inline;
 
+   procedure Backup_Domain_Reset;
+   --  Disable LSE clock and RTC and reset its configurations.
+
+   ---------------------------------------------------------------------------
+   --  Clock Configuration  --------------------------------------------------
+   ---------------------------------------------------------------------------
+
+   ---------------
+   -- HSE Clock --
+   ---------------
+
+   procedure Set_HSE_Clock
+     (Enable     : Boolean;
+      Bypass     : Boolean := False;
+      Enable_CSS : Boolean := False)
+     with Post => HSE_Clock_Enabled = Enable;
+
+   function HSE_Clock_Enabled return Boolean;
+
+   ---------------
+   -- LSE Clock --
+   ---------------
+
+   type HSE_Capability is
+     (Lowest_Drive,
+      Low_Drive,
+      High_Drive,
+      Highest_Drive)
+     with Size => 2;
+
+   procedure Set_LSE_Clock
+     (Enable     : Boolean;
+      Bypass     : Boolean := False;
+      Enable_CSS : Boolean := False;
+      Capability : HSE_Capability)
+     with Post => LSE_Clock_Enabled = Enable;
+
+   function LSE_Clock_Enabled return Boolean;
+
+   ---------------
+   -- HSI Clock --
+   ---------------
+
+   procedure Set_HSI_Clock (Enable : Boolean)
+     with Post => HSI_Clock_Enabled = Enable;
+   --  The HSI clock can't be disabled if it is used directly (via SW mux) as
+   --  system clock or if the HSI is selected as reference clock for PLL with
+   --  PLL enabled (PLLON bit set to ‘1’). It is set by hardware if it is used
+   --  directly or indirectly as system clock.
+
+   function HSI_Clock_Enabled return Boolean;
+
+   -----------------
+   -- HSI48 Clock --
+   -----------------
+
+   procedure Set_HSI48_Clock (Enable : Boolean)
+     with Post => HSI48_Clock_Enabled = Enable;
+
+   function HSI48_Clock_Enabled return Boolean;
+
+   ---------------
+   -- LSI Clock --
+   ---------------
+
+   procedure Set_LSI_Clock (Enable : Boolean)
+     with Post => LSI_Clock_Enabled = Enable;
+
+   function LSI_Clock_Enabled return Boolean;
+
+   ------------------
+   -- System Clock --
+   ------------------
+
+   type SYSCLK_Source is
+     (SYSCLK_SRC_HSI,
+      SYSCLK_SRC_HSE,
+      SYSCLK_SRC_PLL)
+     with Size => 2;
+
+   for SYSCLK_Source use
+     (SYSCLK_SRC_HSI => 2#01#,
+      SYSCLK_SRC_HSE => 2#10#,
+      SYSCLK_SRC_PLL => 2#11#);
+
+   procedure Configure_System_Clock_Mux (Source : SYSCLK_Source);
+
+   -------------------------------
+   -- Domains 1, 2 and 3 Clocks --
+   -------------------------------
+
+   type AHB_Prescaler_Enum is
+     (DIV2,  DIV4,   DIV8,   DIV16,
+      DIV64, DIV128, DIV256, DIV512)
+     with Size => 3;
+
+   type AHB_Prescaler is record
+      Enabled : Boolean := False;
+      Value   : AHB_Prescaler_Enum := AHB_Prescaler_Enum'First;
+   end record with Size => 4;
+
+   for AHB_Prescaler use record
+      Enabled at 0 range 3 .. 3;
+      Value   at 0 range 0 .. 2;
+   end record;
+
+   procedure Configure_AHB_Clock_Prescaler
+     (Value : AHB_Prescaler);
+   --  The AHB clock bus is the CPU clock selected by the AHB prescaler.
+   --  Example to create a variable:
+   --  AHB_PRE  : AHB_Prescaler := (Enabled => True, Value => DIV2);
+
+   type APB_Prescaler_Enum is
+     (DIV2,  DIV4,  DIV8,  DIV16)
+     with Size => 2;
+
+   type APB_Prescaler is record
+      Enabled : Boolean;
+      Value   : APB_Prescaler_Enum := APB_Prescaler_Enum'First;
+   end record with Size => 3;
+
+   for APB_Prescaler use record
+      Enabled at 0 range 2 .. 2;
+      Value   at 0 range 0 .. 1;
+   end record;
+
+   type APB_Clock_Range is (APB_1, APB_2);
+
+   procedure Configure_APB_Clock_Prescaler
+     (Bus   : APB_Clock_Range;
+      Value : APB_Prescaler);
+   --  The APB1 clock bus is the APB1 peripheral clock selected by the APB1
+   --  prescaler.
+   --  The APB2 clock bus is the APB2 peripheral clock selected by the APB2
+   --  prescaler.
+   --  Example to create a variable:
+   --  APB_PRE  : APB_Prescaler := (Enabled => True, Value => DIV2);
+
+   ----------------
+   -- PLL Clocks --
+   ----------------
+
+   type PLL_Source is
+     (PLL_SRC_HSI,
+      PLL_SRC_HSE)
+     with Size => 2;
+
+   for PLL_Source use
+     (PLL_SRC_HSI   => 2#10#,
+      PLL_SRC_HSE   => 2#11#);
+
+   procedure Configure_PLL_Source_Mux (Source : PLL_Source);
+
+   subtype PLLM_Range is Integer range   1 ..  16;
+   subtype PLLN_Range is Integer range   8 .. 127;
+   subtype PLLP_Range is Integer range   2 ..  31;
+   subtype PLLQ_Range is Integer range   2 ..   8
+     with Static_Predicate => (case PLLQ_Range is
+                                 when 2 | 4 | 6 | 8 => True,
+                                 when others => False);
+   subtype PLLR_Range is Integer range   2 ..   8
+     with Static_Predicate => (case PLLR_Range is
+                                 when 2 | 4 | 6 | 8 => True,
+                                 when others => False);
+
+   procedure Configure_PLL
+     (Enable          : Boolean;
+      PLLM            : PLLM_Range;
+      PLLN            : PLLN_Range;
+      PLLP            : PLLP_Range;
+      Enable_Output_P : Boolean;
+      PLLQ            : PLLQ_Range;
+      Enable_Output_Q : Boolean;
+      PLLR            : PLLR_Range;
+      Enable_Output_R : Boolean);
+   --  Configure PLL according with RM0440 rev 6 Chapter 7.2.4 section "PLL"
+   --  pg 282.
+
+   -------------------
+   -- Output Clocks --
+   -------------------
+
+   type MCO_Source is
+     (MCOSEL_Disabled,
+      MCOSEL_SYSCLK,
+      MCOSEL_HSI,
+      MCOSEL_HSE,
+      MCOSEL_PLL,
+      MCOSEL_LSI,
+      MCOSEL_LSE,
+      MCOSEL_HSI48)
+     with Size => 4;
+
+   for MCO_Source use
+     (MCOSEL_Disabled => 2#0000#,
+      MCOSEL_SYSCLK   => 2#0001#,
+      MCOSEL_HSI      => 2#0011#,
+      MCOSEL_HSE      => 2#0100#,
+      MCOSEL_PLL      => 2#0101#,
+      MCOSEL_LSI      => 2#0110#,
+      MCOSEL_LSE      => 2#0111#,
+      MCOSEL_HSI48    => 2#1000#);
+
+   type MCO_Prescaler is
+     (MCOPRE_DIV1,
+      MCOPRE_DIV2,
+      MCOPRE_DIV4,
+      MCOPRE_DIV8,
+      MCOPRE_DIV16)
+     with Size => 3;
+
+   for MCO_Prescaler use
+     (MCOPRE_DIV1  => 2#000#,
+      MCOPRE_DIV2  => 2#001#,
+      MCOPRE_DIV4  => 2#010#,
+      MCOPRE_DIV8  => 2#011#,
+      MCOPRE_DIV16 => 2#100#);
+
+   procedure Configure_MCO_Output_Clock
+     (Source : MCO_Source;
+      Value  : MCO_Prescaler);
+   --  Select the source for micro-controller clock output.
+
+   type LSCO_Source is (LSI, LSE);
+
+   procedure Configure_LSCO_Output_Clock
+     (Enable : Boolean;
+      Source : LSCO_Source := LSCO_Source'First);
+
+   ------------------
+   -- Flash Memory --
+   ------------------
+
+   --  Flash wait states
+   type FLASH_Wait_State is (FWS0, FWS1, FWS2, FWS3, FWS4)
+     with Size => 4;
+
+   procedure Set_FLASH_Latency (Latency : FLASH_Wait_State);
+   --  Constants for Flash Latency
+   --               with VCORE Range 1 boost mode | Range 1 normal mode
+   --  000: Zero wait state    0 < HCLK ≤  34 MHz |   0 < HCLK ≤  30 MHz
+   --  001: One wait state    34 < HCLK ≤  68 MHz |  30 < HCLK ≤  60 MHz
+   --  010: Two wait sates    68 < HCLK ≤ 102 MHz |  60 < HCLK ≤  90 MHz
+   --  011: Three wait sates 102 < HCLK ≤ 136 MHz |  90 < HCLK ≤ 120 MHz
+   --  100: Four wait sates  136 < HCLK ≤ 170 MHz | 120 < HCLK ≤ 150 MHz
+   --  RM0440 STM32G474 pg. 97 chapter 3.3.3 and pg. 129 chapter 3.7.1
+
+   -------------------
+   -- VCORE Scaling --
+   -------------------
+
+   type VCORE_Scaling_Selection is
+     (Range_1,
+      Range_2);
+
+   for VCORE_Scaling_Selection use
+     (Range_1 => 2#01#,
+      Range_2 => 2#10#);
+
+   procedure Set_VCORE_Scaling (Scale : VCORE_Scaling_Selection);
+   --  Range 2 => PLL max. is 26 MHz,
+   --  Range 1 => PLL max. is 150 MHz,
+   --  Range 1 boost => PLL max. is 170 MHz,
+   --  See RM0440 rev 6 Chapter 7.2.8 for frequency x voltage scaling.
+
+   procedure PWR_Overdrive_Enable;
+   --  Range 1 boost mode (R1MODE = 0) when SYSCLK ≤ 170 MHz.
+   --  Range 1 normal mode (R1MODE = 1) when SYSCLK ≤ 150 MHz.
+   --  See RM0440 ver 4 pg. 235 chapter 6.1.5 for other modes.
+
 end STM32.RCC;
